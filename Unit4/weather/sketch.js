cde245979d787daa5f67be8c323a1f689def4467
nameztill <telmuun.batjargal@yahoo.com>
2022-04-26 12:06:38 -0500

    a

--- Unit4/weather/sketch.js
+++ Unit4/weather/sketch.js
@@ -73,22 +73,52 @@ function draw() {
       translate(x, 0);
       fill("white");
       noStroke();
+      //1st cloud
+      ellipse(65,40,100,30);
+      ellipse(10,40,100,50);
+      ellipse(15,60,140,20);
+      ellipse(1,55,260,30);
 
-      push();
-      translate(100, 50);
-      ellipse(400, 100, 130, 30);
-      ellipse(337, 112, 130, 10);
-      pop();
-      ellipse(318, 295, 130, 50);
-      ellipse(390, 270, 130, 60);
-      ellipse(381, 296, 130, 40);
-      ellipse(50, 260, 200, 40);
-      ellipse(23, 218, 130, 70);
-      ellipse(121, 376, 200, 70);
-      ellipse(209, 398, 130, 40);
-      ellipse(141, 345, 60, 30);
-      ellipse(262, 313, 80, 10);
-      pop();
+
+      // 2nd cloud
+      ellipse(337,112,130,20);
+      ellipse(442,117,130,20);
+      ellipse(400,100,130,40);
+      ellipse(379,118,130,20);
+
+      //3rd cloud
+      ellipse(50,260,200,40);
+      ellipse(23,218,130,70);
+
+      //4th cloud
+      ellipse(318,295,130,50);
+      ellipse(262,313,80,10);
+      ellipse(370,270,130,60);
+      ellipse(405,313,200,10);
+      ellipse(420,296,130,40);
+
+      //5th cloud
+      ellipse(53,395,230,40);
+      ellipse(209,400,230,20);
+      ellipse(90,376,150,70);
+      ellipse(131,345,90,35);
+      ellipse(190,375,120,55);
+
+      // push();
+      // translate(100, 50);
+      // ellipse(400, 100, 130, 30);
+      // ellipse(337, 112, 130, 10);
+      // pop();
+      // ellipse(318, 295, 130, 50);
+      // ellipse(390, 270, 130, 60);
+      // ellipse(381, 296, 130, 40);
+      // ellipse(50, 260, 200, 40);
+      // ellipse(23, 218, 130, 70);
+      // ellipse(121, 376, 200, 70);
+      // ellipse(209, 398, 130, 40);
+      // ellipse(141, 345, 60, 30);
+      // ellipse(262, 313, 80, 10);
+      // pop();
       // move the cloud's x position
       x = x + windspeed / 3;
       if (x > width) x = 0;
